1590e44db14871ceee7d867c0d26982b09a06859
DevanStrife <113341203+DevanStrife@users.noreply.github.com>
2024-06-25 13:16:44 +0200

    comment :)

--- Backend_B2A.py
+++ Backend_B2A.py
@@ -1396,6 +1396,7 @@ def get_all_user_appointments(user_id):
         rows = cur.fetchall()
         column_names = [desc[0] for desc in cur.description]
 
+        # old code but keeping it incase we need to revert
         #data = [dict(zip(column_names, row)) for row in rows]
         data = [{key: row[key] for key in column_names if key in row} for row in rows]
 
